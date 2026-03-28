5f8ac9a6c2b0b0e66c3d387837736c0dd468c929
fionn mcgoldrick <fionnmc123@gmail.com>
2026-03-28 23:13:37 +0000

    Fix cancel fishing mini-game bug and add new note

--- Assets/Scripts/Player/PlayerStateMachine/PlayerFishingState.cs
+++ Assets/Scripts/Player/PlayerStateMachine/PlayerFishingState.cs
@@ -43,7 +43,7 @@ public class PlayerFishingState : PlayerState
             previousPhase = phase;
         }
 
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && !minigameActive)
         {
             player.ExitFishing();
             return;
